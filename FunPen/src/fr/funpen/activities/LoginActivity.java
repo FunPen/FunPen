41a3cf0536d37343f5b046c47e1954caa22fd614
ReaperSoon <cohensteve@hotmail.fr>
2015-01-13 20:44:07 +0800

    Update login activity (countdown) Add gallery

--- FunPen/src/fr/funpen/activities/LoginActivity.java
+++ FunPen/src/fr/funpen/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package fr.funpen.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,39 +11,54 @@ import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
+	private final int MILLISECONDS = 30000;
+	private final int COUNTDOWN_INTERVAL = 1000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 	}
-	
+
 	public void onLoginClicked(View v) {
 		Log.i("FunPen", "Login clicked");
 		String email = ((EditText) findViewById(R.id.field_email)).getText().toString();
 		String password = ((EditText) findViewById(R.id.field_password)).getText().toString();
-		
-		if (email.isEmpty()) {
-			Toast.makeText(getApplicationContext(), "Both field must be filled", Toast.LENGTH_SHORT).show();
-		}else if (password.isEmpty()) {
+
+		if (email.isEmpty() || password.isEmpty()) {
 			Toast.makeText(getApplicationContext(), "Both field must be filled", Toast.LENGTH_SHORT).show();
 		}else {
 			// TODO Send server the authentification request
+			// For now, accept anything
+			
 		}
 	}
-	
+
 	public void onGetPasswordClicked(View v) {
-		Log.i("FunPen", "GetPassword clicked");
 		String email = ((EditText) findViewById(R.id.field_email)).getText().toString();
 		if (email.isEmpty()) {
 			Toast.makeText(getApplicationContext(), "Please fill the blank with a valid email", Toast.LENGTH_SHORT).show();
 		}else {
-			Log.i("FunPen", "Email is : " + email);
+			changePasswordButtonTextOverTimer();
 			// TODO Send server the email request
-			// TODO 30 seconds clock on the button text (anti spam)
-			Button getPassword = ((Button) findViewById(R.id.button_password));
-			getPassword.setEnabled(false);
-			getPassword.setText(getPassword.getText() + " (30)");
 			Toast.makeText(getApplicationContext(), "Please check your email to get the password", Toast.LENGTH_SHORT).show();
 		}
+	}
+
+	private void changePasswordButtonTextOverTimer() {
+		final Button getPasswordButton = ((Button) findViewById(R.id.button_password));
+		final String initialText = getPasswordButton.getText().toString();
+
+		new CountDownTimer(MILLISECONDS, COUNTDOWN_INTERVAL) {       
+			public void onTick(long millisUntilFinished) {   
+				getPasswordButton.setEnabled(false);
+				getPasswordButton.setText(initialText + " (" + (millisUntilFinished / 1000) + ")");      
+			}       
+
+			public void onFinish() {         
+				getPasswordButton.setText(initialText);
+				getPasswordButton.setEnabled(true);
+			}   
+		}.start();
 	}
 }
